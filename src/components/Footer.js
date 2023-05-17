dcbbc7ff1af82b40bc9260b4a23149d2f7c5448d
Gabriel Carvalho Goncalves <gabrielcarvalhooficial@gmail.com>
2023-05-17 01:27:24 -0300

    Footer atualizado

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import style from "./Footer.module.css";
 function Footer() {
     return ( 
         <footer>
-            <p>Projeto realizado por Gabriel C. Gonçalves. Modelo baseado no layout de <a target="blank_" href="https://dribbble.com/shots/9631225--Design-for-DailyUi-Day004-Calculator">Siarhei Vysotski</a></p>
+            <p>Projeto realizado por Gabriel C. Gonçalves. Modelo baseado no layout de <a target="blank_" href="https://dribbble.com/shots/9631225--Design-for-DailyUi-Day004-Calculator">Siarhei Vysotski</a>.</p>
         </footer>
      );
 }
